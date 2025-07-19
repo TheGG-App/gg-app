d2bcc84e8d2e3ff36a534cea01da9fbc34917682
Poobcraft <justintianopc@gmail.com>
2025-07-19 03:33:51 -0400

    fixing firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,9 @@
 // src/config/firebase.js
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
-// TODO: Replace with your Firebase config from console.firebase.google.com
 const firebaseConfig = {
   apiKey: "AIzaSyBwf7PrhN45My-mDjNjC7o_dTJ-07cGAAg",
   authDomain: "gg-recipe-app.firebaseapp.com",
@@ -13,12 +13,7 @@ const firebaseConfig = {
   appId: "1:845280238042:web:f73e37f84b8b75568f88e3"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Auth
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-
-// Firestore Database
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
